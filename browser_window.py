572b8d6d016e70b0f9b617c9fb2d82aebe6e2b76
The game fire <thegamefire@outlook.com>
2025-05-06 23:40:44 +0200

    First commit

---
+++ browser_window.py
@@ -0,0 +1,14 @@
+from PySide6 import QtCore, QtWidgets, QtWebEngineWidgets
+
+
+class BrowserWindow():
+    def __init__(self):
+        super().__init__()
+
+        self.browser = QtWebEngineWidgets.QWebEngineView()
+        self.browser.load("https://reactive.fugi.tech/group")
+        self.browser.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground)
+        self.browser.setWindowFlag(QtCore.Qt.WindowType.FramelessWindowHint)
+        self.browser.setStyleSheet("QWidget {background:transparent;}")
+        self.browser.show()
+
